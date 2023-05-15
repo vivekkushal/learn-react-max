fa3166bf9b0e082e070351d0b750fa41c08cd285
vivekkushal <vivekjss1@gmail.com>
2023-05-15 12:14:20 +0530

    chore: Tab as 2 spaces

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.date} />
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-        </Card>
-    );
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React', title);
+
+  const clickHandler = () => {
+    setTitle('Updated');
+    console.log(title);
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
 };
 
 export default ExpenseItem;
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Card.css';
 
 const Card = (props) => {
-    const classes = 'card ' + props.className;
+  const classes = 'card ' + props.className;
 
-    return <div className={classes}>{props.children}</div>;
+  return <div className={classes}>{props.children}</div>;
 };
 
 export default Card;
